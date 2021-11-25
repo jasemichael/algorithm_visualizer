ce912cb6697d00b2741d64128b2efcb09a432ba8
Jase M. Wilson <jasemwilson7@gmail.com>
2021-11-24 18:49:05 -0700

    Complete algorithm menu item

--- src/components/AlgorithmMenuItem.tsx
+++ src/components/AlgorithmMenuItem.tsx
@@ -1,15 +1,42 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 interface Props {
     title: string,
     imageSrc: string, 
-    imageAlt: string
+    imageAlt: string,
+    redirectTo: string
 }
 
-const AlgorithmMenuItem = ({title, imageSrc, imageAlt}: Props) => {
+const AlgorithmMenuItem = ({title, imageSrc, imageAlt, redirectTo}: Props) => {
+
+    const StyledLink = styled(Link)`
+        color: black;
+        text-decoration: none;
+        border: black solid 2px;
+        border-radius: 8px;
+        &:hover {
+            transform: scale(1.05, 1.05);
+            transition: 0.25s ease-in-out;
+        }
+    `;
+    const H2 = styled.h2`
+        text-align: center;
+    `;
+
+    const Img = styled.img`
+        margin: auto;
+        width: 20rem;
+        display: block;
+    `;
+
     return (
-        <div>
-            <p>{title}</p>
-            <img src={imageSrc} alt={imageAlt} />
-        </div>
+        <StyledLink to={redirectTo}>
+            <div>
+                <H2>{title}</H2>
+                <Img src={imageSrc} alt={imageAlt} />
+            </div>
+        </StyledLink>
     );
 }
 
