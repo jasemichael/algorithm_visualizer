6f17f115a66b5282a9067f7ec85b352d7ac16985
Jase M. Wilson <jasemwilson7@gmail.com>
2021-11-24 21:08:57 -0700

    Start bubble sort page

---
+++ src/pages/BubbleSortPage.tsx
@@ -0,0 +1,11 @@
+import { Page } from "../components";
+
+const BubbleSortPage = () => {
+    return (
+        <Page title="Bubble Sort">
+
+        </Page>
+    );
+}
+
+export default BubbleSortPage;
