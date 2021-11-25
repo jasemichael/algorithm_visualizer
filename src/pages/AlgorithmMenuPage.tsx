54639a5e68a5838e8249ada0fa82f43131b7efc7
Jase M. Wilson <jasemwilson7@gmail.com>
2021-11-24 18:49:29 -0700

    Complete algorithm menu page

--- src/pages/AlgorithmMenuPage.tsx
+++ src/pages/AlgorithmMenuPage.tsx
@@ -1,11 +1,35 @@
-import AlgorithmMenuItem from "../components/AlgorithmMenuItem";
+import {AlgorithmMenuItem} from '../components';
 import sortingImage from "../assets/sorting_algorithm.jpg";
+import bstImage from "../assets/bst_algorithm.png";
+import graphImage from "../assets/graph_algorithm.png";
+import styled from "styled-components";
 
 const AlgorithmMenuPage = () => {
-    
+    const H1 = styled.h1`
+        text-align: center;
+    `;
+
+    const Algorithms = styled.div`
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        column-gap: 20px;
+        row-gap: 2em;
+        width: 90%;
+        margin: auto;
+    `;
+
+
     return (
         <div>
-            <AlgorithmMenuItem title="Sorting" imageSrc={sortingImage} imageAlt="Sorting algorithm"/>
+            <H1>Algorithms</H1>
+            <Algorithms>
+                <AlgorithmMenuItem title="Sorting" imageSrc={sortingImage} imageAlt="Sorting algorithms" redirectTo="/sorting"/>
+                <AlgorithmMenuItem title="Binary Search Tree" imageSrc={bstImage} imageAlt="BST algorithms" redirectTo="/bst"/>
+                <AlgorithmMenuItem title="Graphs" imageSrc={graphImage} imageAlt="Graph algorithms" redirectTo="/graph"/>
+                <AlgorithmMenuItem title="Sorting" imageSrc={sortingImage} imageAlt="Sorting algorithms" redirectTo="/sorting"/>
+                <AlgorithmMenuItem title="Binary Search Tree" imageSrc={bstImage} imageAlt="BST algorithms" redirectTo="/bst"/>
+                <AlgorithmMenuItem title="Graphs" imageSrc={graphImage} imageAlt="Graph algorithms" redirectTo="/graph"/>
+            </Algorithms>
         </div>
     );
 }
