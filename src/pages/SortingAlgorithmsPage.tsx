0e55c079a60b58d8e875323c0909f2b2a89fc5e7
Jase M. Wilson <jasemwilson7@gmail.com>
2021-11-24 21:04:12 -0700

    Complete sorting algorithms page

--- src/pages/SortingAlgorithmsPage.tsx
+++ src/pages/SortingAlgorithmsPage.tsx
@@ -1,9 +1,24 @@
+import { Page, Menu, MenuItem } from '../components';
+import { 
+    bubbleSortImage, 
+    insertionSortImage, 
+    selectionSortImage, 
+    mergeSortImage,
+    shellSortImage,
+    quickSortImage
+} from '../assets';
 const SortingAlgorithmsPage = () => {
-
     return (
-        <div>
-            
-        </div>
+        <Page title='Sorting Algorithms'>
+            <Menu header="Sorting Algorithms">
+                <MenuItem title="Bubble Sort" imageSrc={bubbleSortImage} imageAlt="Bubble Sort" redirectTo="bubble" />
+                <MenuItem title="Insertion Sort" imageSrc={insertionSortImage} imageAlt="Insertion Sort" redirectTo="insertion" />
+                <MenuItem title="Selection Sort" imageSrc={selectionSortImage} imageAlt="Selection Sort" redirectTo="selection" />
+                <MenuItem title="Merge Sort" imageSrc={mergeSortImage} imageAlt="Merge Sort" redirectTo="merge" />
+                <MenuItem title="Shell Sort" imageSrc={shellSortImage} imageAlt="Shell Sort" redirectTo="shell" />
+                <MenuItem title="Quick Sort" imageSrc={quickSortImage} imageAlt="Quick Sort" redirectTo="quick" />
+            </Menu>
+        </Page>
     )
 }
 
