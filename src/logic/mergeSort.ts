99f4e69071df403e56de5f5994891b4a317c16da
Jase M. Wilson <jasemwilson7@gmail.com>
2021-11-27 15:06:25 -0700

    Complete merge sort implementation

--- src/logic/mergeSort.ts
+++ src/logic/mergeSort.ts
@@ -2,8 +2,25 @@ function mergeSort(array: {number: number, active: boolean}[]) {
     if (array.length === 1) {
         return array
     }
-    mergeSort(array.slice(0, array.length/2));
-    mergeSort(array.slice(array.length/2, array.length));
+    var left = mergeSort(array.slice(0, array.length/2));
+    var right = mergeSort(array.slice((array.length/2)+1, array.length));
+    var newArray = [];
+    while(left?.length !== 0 && right?.length !== 0) {
+        if (left[0] > right[0]) {
+            newArray.push(right?.shift());
+        } else {
+            newArray.push(left?.shift());
+        }
+    }
+
+    while(left?.length !== 0) {
+        newArray.push(left?.shift());
+    }
+
+    while(right?.length !== 0) {
+        newArray.push(right?.shift());
+    }
+    return array;
 }
 
 export default mergeSort;
