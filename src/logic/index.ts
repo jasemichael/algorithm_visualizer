e70a395d57d1cd0c4e2e1d227beb13d7a6ea0d69
Jase M. Wilson <jasemwilson7@gmail.com>
2021-11-27 16:28:28 -0700

    Add merge sort and page to index

--- src/logic/index.ts
+++ src/logic/index.ts
@@ -3,3 +3,4 @@ export {default as randomize} from './randomize';
 export {default as bubbleSort} from './bubbleSort';
 export {default as insertionSort} from './insertionSort';
 export {default as selectionSort} from './selectionSort';
+export {default as mergeSort} from './mergeSort';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export {default as SortingAlgorithmsPage} from './SortingAlgorithmsPage';
 export {default as BubbleSortPage} from './BubbleSortPage';
 export {default as InsertionSortPage} from './InsertionSortPage';
 export {default as SelectionSortPage} from './SelectionSortPage';
+export {default as MergeSortPage} from './MergeSortPage';
