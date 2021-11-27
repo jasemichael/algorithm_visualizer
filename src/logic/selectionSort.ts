7396e3acb708896f2a8caf8bb1fdd9cc9b8d41c6
Jase M. Wilson <jasemwilson7@gmail.com>
2021-11-27 14:46:47 -0700

    add queue feature to selection sort

--- src/logic/selectionSort.ts
+++ src/logic/selectionSort.ts
@@ -1,17 +1,19 @@
-function selectionSort(array: {number: number, active: boolean}[], setArray: React.Dispatch<React.SetStateAction<{number: number, active: boolean}[]>>) {
+function selectionSort(array: {number: number, active: boolean}[]) {
+    var queue = [];
     for(var i=0; i<array.length; i++) {
         // find smallest element's index
         var minIndex = i;
         for(var j=i; j<array.length; j++) { 
-            if (array[j]["number"] > array[minIndex]["number"]) {
+            if (array[j]["number"] < array[minIndex]["number"]) {
                 minIndex = j;
             }
         }
         var element = array[i];
         array[i] = array[minIndex];
         array[minIndex] = element;
+        queue.push([...array])
     }
-    return array;
+    return queue;
 }
 
 export default selectionSort;
