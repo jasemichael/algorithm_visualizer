33a55cb2655f3408b9f3d7ac0c7cfcb7b9c5ee80
Jase M. Wilson <jasemwilson7@gmail.com>
2021-12-01 00:04:30 -0700

    Add merge sort and finish implementation

--- src/logic/index.ts
+++ src/logic/index.ts
@@ -1,5 +1,6 @@
 export {default as createArrayFromNum} from './createArrayFromNum';
 export {default as randomize} from './randomize';
+export {default as sleep} from './sleep';
 export {default as bubbleSort} from './bubbleSort';
 export {default as insertionSort} from './insertionSort';
 export {default as selectionSort} from './selectionSort';
--- src/logic/mergeSort.ts
+++ src/logic/mergeSort.ts
@@ -1,26 +1,31 @@
-function mergeSort(array: {number: number, active: boolean}[]) {
+import { Dispatch, SetStateAction } from "react";
+import { sleep } from '.';
+
+async function mergeSort(array: {number: number, active: boolean}[], setArray: Dispatch<SetStateAction<{number: number, active: boolean}[]>>) {
     if (array.length === 1) {
         return array
     }
-    var left = mergeSort(array.slice(0, array.length/2));
-    var right = mergeSort(array.slice((array.length/2)+1, array.length));
+    var left: any[] = await mergeSort(array.slice(0, array.length/2), setArray);
+    var right: any[] = await mergeSort(array.slice(array.length/2, array.length), setArray);
     var newArray = [];
-    while(left?.length !== 0 && right?.length !== 0) {
-        if (left[0] > right[0]) {
-            newArray.push(right?.shift());
+    while(left.length !== 0 && right.length !== 0) {
+        if (left[0]["number"] > right[0]["number"]) {
+            newArray.push(right.shift());
         } else {
-            newArray.push(left?.shift());
+            newArray.push(left.shift());
         }
     }
 
-    while(left?.length !== 0) {
-        newArray.push(left?.shift());
+    while(left.length !== 0) {
+        newArray.push(left.shift());
     }
 
-    while(right?.length !== 0) {
-        newArray.push(right?.shift());
+    while(right.length !== 0) {
+        newArray.push(right.shift());
     }
-    return array;
+    setArray(newArray);
+    await sleep(1);
+    return newArray;
 }
 
 export default mergeSort;
